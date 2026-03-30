05203b1b0dc929e25e879fb31e7f4c4f9f98be57
adityakaiser <singhadityakaiser@gmail.com>
2026-03-30 20:49:12 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,24 @@
+a=20
+if(type(a)==int):
+    print("a is an integer")
+else:
+    print("a is not an integer")
+
+
+
+a=20
+if(type(a)==float):
+    print("a is a float value")
+else:
+    print("a is not a float value")
+
+
+a=5
+b=5
+if(a is b):
+    print("x and y are the same IDENTITY")
+
+a=20
+b=30
+if(a is not b):
+    print("x and y are different IDENTITIES")        
---
+++ file2.py
@@ -0,0 +1,15 @@
+a=5
+b=-4
+
+#print bitwise right shift operator
+
+print("a>>1",a>>1)
+print("b>>1",b>>1)
+
+a=-10
+b=30
+
+#print bitwise left shift operator
+
+print("a<<1",a<<1)
+print("b<<1",b<<1)
